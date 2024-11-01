6f063fc6d55ded2d0a93ece6205988549bb221cf
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2024-11-01 22:49:39 +0300

    Click events update

--- click-events/src/Button.jsx
+++ click-events/src/Button.jsx
@@ -1,11 +1,18 @@
 function Button() {
 
-  const handleClick = () => console.log("OUCH!");
+  let count = 0;
 
-const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+  const handleClick = (name) => {
+    if (count < 3) {
+      count++;
+      console.log(`${name} you clicked me ${count} time/s`);
+    } else {
+      console.log(`${name} stop cliking me!`);
+    }
+  }
 
   return (
-    <button onClick={() => handleClick2("Bro")}>Click Me 😊</button>
+    <button onClick={() => handleClick("Bro")}>Click Me 😊</button>
   );
 
 }
