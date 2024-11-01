3ba4ac27156a077138db19a125a44e65dcbd87ea
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2024-11-01 10:08:36 +0300

    Render list update 2

--- render-lists/src/List.jsx
+++ render-lists/src/List.jsx
@@ -1,5 +1,12 @@
 function List() {
 
+
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "orange", calories: 45 },
+    { id: 3, name: "banana", calories: 105 }
+  ];
+
   // fruits.sort((a, b) => a.name.localeCompare(b.name)) //Alfabetik sıralama için
   fruits.sort((a, b) => a.calories - b.calories) //Sayıya göre sıralama için
 
