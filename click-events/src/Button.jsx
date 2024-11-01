b94c8914ab52d2b288a7f2868dadad8f95ade114
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2024-11-01 22:46:33 +0300

    Click events

---
+++ click-events/src/Button.jsx
@@ -0,0 +1,13 @@
+function Button() {
+
+  const handleClick = () => console.log("OUCH!");
+
+const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+
+  return (
+    <button onClick={() => handleClick2("Bro")}>Click Me 😊</button>
+  );
+
+}
+
+export default Button;
