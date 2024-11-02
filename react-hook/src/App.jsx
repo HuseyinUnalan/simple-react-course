84833c15119e7ab17a61b31d0cb7dee95735db1c
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2024-11-02 10:01:27 +0300

    React hook update

--- react-hook/src/App.jsx
+++ react-hook/src/App.jsx
@@ -1,12 +1,14 @@
 
 import MyComponent from "./MyComponent"
+import Counter from "./Counter"
 
 function App() {
 
 
   return (
     <>
-      <MyComponent />
+      {/* <MyComponent /> */}
+      <Counter />
     </>
   )
 }
