f5f73ef6c56416401d8d394a03ecd6f6572a625c
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2024-11-03 13:15:43 +0300

    React updater functions

---
+++ react-updater-functions/src/MyComponent.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(c => c + 1);
+    setCount(c => c + 1);
+    setCount(c => c + 1);
+  }
+
+  function decrement() {
+    setCount(c => c - 1);
+    setCount(c => c - 1);
+    setCount(c => c - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+
+    <>
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={decrement}>Decerement</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={increment}>Increment</button>
+      </div>
+    </>
+
+  );
+}
+
+export default MyComponent
